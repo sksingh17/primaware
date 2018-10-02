8370c01b48261e39fdd38f71a3f55b2fe04f03d9
subham singh <singhsubham92@gmail.com>
2018-10-02 19:16:26 +0530

    cors addition

--- server/app/app.js
+++ server/app/app.js
@@ -11,6 +11,10 @@ app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  if(res=='OPTIONS'){
+    res.header("Access-Control-Allow-Methods",'GET,POST,PUT,PATCH,DELETE');
+    return res.json({});
+  }
   next();
 });
 
